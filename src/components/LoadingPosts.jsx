5818310cdddfa0b8881eaf156384c85dc899c11a
MohamadMilly <volassoa@gmail.com>
2026-01-02 12:20:42 +0300

    added loading page for posts

---
+++ src/components/LoadingPosts.jsx
@@ -0,0 +1,58 @@
+export function LoadingPosts() {
+  return (
+    <section className="flex flex-col gap-2">
+      <article className="flex flex-wrap rounded-2xl min-37.5 overflow-hidden bg-slate-900/70 backdrop-blur-2xl relative">
+        <div className="w-full h-60 sm:w-37.5 sm:h-37.5 shrink-0 flex justify-center items-center bg-gray-600/10 animate-pulse"></div>
+        <aside className="grow px-4 py-3 basis-xs relative">
+          <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
+          <div>
+            <span className="w-20 py-1 bg-gray-600/10 rounded animate-pulse"></span>
+          </div>
+          <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+          </div>
+        </aside>
+      </article>
+      <article className="flex flex-wrap rounded-2xl min-37.5 overflow-hidden bg-slate-900/70 backdrop-blur-2xl relative">
+        <div className="w-full h-60 sm:w-37.5 sm:h-37.5 shrink-0 flex justify-center items-center bg-gray-600/10 animate-pulse"></div>
+        <aside className="grow px-4 py-3 basis-xs relative">
+          <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
+          <div>
+            <span className="w-20 py-1 bg-gray-600/10 rounded animate-pulse"></span>
+          </div>
+          <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+          </div>
+        </aside>
+      </article>
+      <article className="flex flex-wrap rounded-2xl min-37.5 overflow-hidden bg-slate-900/70 backdrop-blur-2xl relative">
+        <div className="w-full h-60 sm:w-37.5 sm:h-37.5 shrink-0 flex justify-center items-center bg-gray-600/10 animate-pulse"></div>
+        <aside className="grow px-4 py-3 basis-xs relative">
+          <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
+          <div>
+            <span className="w-20 py-1 bg-gray-600/10 rounded animate-pulse"></span>
+          </div>
+          <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+          </div>
+        </aside>
+      </article>
+      <article className="flex flex-wrap rounded-2xl min-37.5 overflow-hidden bg-slate-900/70 backdrop-blur-2xl relative">
+        <div className="w-full h-60 sm:w-37.5 sm:h-37.5 shrink-0 flex justify-center items-center bg-gray-600/10 animate-pulse"></div>
+        <aside className="grow px-4 py-3 basis-xs relative">
+          <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
+          <div>
+            <span className="w-20 py-1 bg-gray-600/10 rounded animate-pulse"></span>
+          </div>
+          <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+          </div>
+        </aside>
+      </article>
+    </section>
+  );
+}
--- src/routes/PostsDashboard.jsx
+++ src/routes/PostsDashboard.jsx
@@ -1,34 +1,37 @@
 import { useMyPosts } from "../contexts/myPostsContext";
 import { PostPreviewCard } from "../components/PostPreviewCard";
 import { SearchBar } from "../components/SearchBar";
+import { LoadingPosts } from "../components/LoadingPosts";
 export function PostsDashboard() {
   const { isLoading, error, visiblePosts } = useMyPosts();
   if (error) return <p>Error: {error}</p>;
 
-  if (isLoading) return <p>Loading...</p>;
-
   return (
     <main className="w-full px-4 sm:px-12 py-4 my-6 max-w-190 mx-auto text-gray-200 transition-all duration-300">
       <SearchBar />
-      <section className="flex flex-col gap-2">
-        {visiblePosts && visiblePosts.length !== 0 ? (
-          visiblePosts.map((post) => {
-            return (
-              <PostPreviewCard
-                key={post.id}
-                title={post.title}
-                createdAt={post.createdAt}
-                updatedAt={post.updatedAt}
-                featuredImageURL={post.featuredImageURL}
-                published={post.published}
-                slug={post.slug}
-              />
-            );
-          })
-        ) : (
-          <p className="text-center text-sm mask-t-from-1.5">No posts.</p>
-        )}
-      </section>
+      {isLoading ? (
+        <LoadingPosts />
+      ) : (
+        <section className="flex flex-col gap-2">
+          {visiblePosts && visiblePosts.length !== 0 ? (
+            visiblePosts.map((post) => {
+              return (
+                <PostPreviewCard
+                  key={post.id}
+                  title={post.title}
+                  createdAt={post.createdAt}
+                  updatedAt={post.updatedAt}
+                  featuredImageURL={post.featuredImageURL}
+                  published={post.published}
+                  slug={post.slug}
+                />
+              );
+            })
+          ) : (
+            <p className="text-center text-sm mask-t-from-1.5">No posts.</p>
+          )}
+        </section>
+      )}
     </main>
   );
 }
