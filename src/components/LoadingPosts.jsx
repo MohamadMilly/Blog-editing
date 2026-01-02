bcd898186d90c3a0adbf0baee21a5ba784fc02b8
MohamadMilly <volassoa@gmail.com>
2026-01-02 12:26:04 +0300

    copied the inhanced loading post card

--- src/components/LoadingPosts.jsx
+++ src/components/LoadingPosts.jsx
@@ -19,7 +19,7 @@ export function LoadingPosts() {
         <aside className="grow px-4 py-3 basis-xs relative">
           <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
           <div>
-            <span className="w-20 py-1 bg-gray-600/10 rounded animate-pulse"></span>
+            <span className="w-20 py-2 bg-gray-600/10 rounded animate-pulse block"></span>
           </div>
           <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
             <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
@@ -32,7 +32,7 @@ export function LoadingPosts() {
         <aside className="grow px-4 py-3 basis-xs relative">
           <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
           <div>
-            <span className="w-20 py-1 bg-gray-600/10 rounded animate-pulse"></span>
+            <span className="w-20 py-2 bg-gray-600/10 rounded animate-pulse block"></span>
           </div>
           <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
             <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
@@ -45,7 +45,20 @@ export function LoadingPosts() {
         <aside className="grow px-4 py-3 basis-xs relative">
           <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
           <div>
-            <span className="w-20 py-1 bg-gray-600/10 rounded animate-pulse"></span>
+            <span className="w-20 py-2 bg-gray-600/10 rounded animate-pulse block"></span>
+          </div>
+          <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+            <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
+          </div>
+        </aside>
+      </article>
+      <article className="flex flex-wrap rounded-2xl min-37.5 overflow-hidden bg-slate-900/70 backdrop-blur-2xl relative">
+        <div className="w-full h-60 sm:w-37.5 sm:h-37.5 shrink-0 flex justify-center items-center bg-gray-600/10 animate-pulse"></div>
+        <aside className="grow px-4 py-3 basis-xs relative">
+          <h2 className="text-lg font-medium mb-1 p-4 bg-gray-600/10 animate-pulse rounded"></h2>
+          <div>
+            <span className="w-20 py-2 bg-gray-600/10 rounded animate-pulse block"></span>
           </div>
           <div className="text-sm mt-2 flex items-center gap-x-6 text-gray-400 flex-wrap">
             <div className="bg-gray-600/10 w-20 py-1 animate-pulse rounded"></div>
